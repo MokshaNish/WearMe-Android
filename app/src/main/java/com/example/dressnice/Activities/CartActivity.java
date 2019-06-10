6f9fa6638a3b8a91cdbd2f707423f9e1ce2120bf
mokshagit config --global user.name moksha <cb007087@students.apiit.lk>
2019-06-10 08:31:32 +0530

    layouts

--- app/src/main/java/com/example/dressnice/Activities/CartActivity.java
+++ app/src/main/java/com/example/dressnice/Activities/CartActivity.java
@@ -1,11 +1,13 @@
 package com.example.dressnice.Activities;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -96,5 +98,10 @@ public class CartActivity extends AppCompatActivity {
             total += price * qty;
         }
         totalprice.setText(String.valueOf(total));
+    }
+
+    public void checkout(View view) {
+        Intent intent = new Intent(CartActivity.this, CheckoutActivity.class);
+        startActivity(intent);
     }
 }
